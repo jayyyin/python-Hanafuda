3658b822ac7e09c3012effa3c1825f2636a1993d
jayyin <jayyin11043@hotmail.com>
2016-06-05 05:14:25 +0800

    lots of work done

---
+++ Hanafuda.py
@@ -0,0 +1,113 @@
+'''
+   This is an experimental game 
+'''
+
+# I - IMPORT AND INITIALIZE
+import pygame, hanafuda_sprites, random
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((1024,768))
+pySprites = hanafuda_sprites
+
+# Create a list of Joystick objects.
+joysticks = []
+for joystick_no in range(pygame.joystick.get_count()):
+    stick = pygame.joystick.Joystick(joystick_no)
+    stick.init()
+    joysticks.append(stick)
+    
+def main():
+    '''This function defines the 'mainline logic' for our game.'''
+      
+    # DISPLAY
+    pygame.display.set_caption("Hanafuda prototype")
+    
+    background = pygame.image.load("images\\background\\Tatami.JPG")
+    
+    screen.blit(background, (0, 0))
+    
+    
+    
+    # ENTITIES 
+    buttonsSS = pySprites.ScreenSection(screen.get_width()/2 -200, screen.get_height()/2 -100, 
+                                         screen.get_width()/2 + 200, screen.get_height() -100)
+    
+    buttons = []
+    player1_hand =[]
+    player1_score = []
+    player2_hand = []
+    player2_score = []
+    gameboard = []
+    deck = []
+    quitButton = pySprites.Button(screen,buttonsSS, "Quit")
+    buttons.append(quitButton)
+    
+    #the "base line" for the ground
+    #topy = screen.get_height()- 200
+    
+    
+    #while len(ground) < screen.get_width():
+        
+        #hill_chance = random.randrange(0, 18)
+        ##if hill chance is anything but 0 it will generate a flat ground, else it will generate a hill for the next couple pixels
+        #if (hill_chance != 0):
+            #ground.append(pySprites.Ground(screen,leftx, topy))
+            #leftx =leftx + 1
+        #else:
+            ## generating random hill sizes each time
+            #hill_size = random.randrange(5,15,2)
+            ##first loop for left side up to top of hill   
+            #for pixel in range(1,hill_size, 1):
+                ##a "less than 1" exponent for rounding effect of the hill
+                #ground.append(pySprites.Ground(screen,leftx, topy- (pixel**0.85)))
+                #leftx =leftx + 1
+            ##2nd loop for the top of hill flat section
+            #for pixel in range(1,int(hill_size/1.5)):
+                #ground.append(pySprites.Ground(screen,leftx, topy- hill_size**0.85))
+                #leftx = leftx + 1
+            ##3rd loop for the right half of the hill
+            #for pixel in range(hill_size, 0, -1):
+                #ground.append(pySprites.Ground(screen,leftx, topy- (pixel**0.85)))
+                #leftx =leftx + 1
+                
+    #groundgroup = pygame.sprite.Group(ground)
+    buttonGroup = pygame.sprite.Group(buttons)
+    # ASSIGN
+    clock = pygame.time.Clock()
+    keepGoing = True
+    
+    # Hide the mouse pointer
+    #pygame.mouse.set_visible(False)
+    
+    # LOOP
+    while keepGoing:
+        
+        # TIME
+        clock.tick(30)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                keepGoing = False        
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mousex,mousey = pygame.mouse.get_pos()
+                print quitButton.within( mousex, mousey)
+                if quitButton.within(mousex, mousey):
+                    print "quit"
+                    keepGoing = False
+            
+        
+        # REFRESH SCREEN
+        #screen.blit(background, (0, 0))
+        #allSprites.update()
+        #allSprites.draw(screen)   
+        #groundgroup.update()
+        #groundgroup.draw(screen)
+        buttonGroup.draw(screen)
+        pygame.display.flip()
+    
+    
+    pygame.mouse.set_visible(True)
+    pygame.time.delay(3000)
+    pygame.quit()
+        
+main()
--- hanafuda_sprites.py
+++ hanafuda_sprites.py
@@ -14,6 +14,17 @@ import math
 import random
 import time
 
+class ScreenSection():
+    '''this will contain information like x and y bounding boxes'''
+    def __init__(self, topLeftX, topLeftY, bottomRightX, bottomRightY):
+        self.tLX = topLeftX
+        self.tLY = topLeftY
+        self.bRX = bottomRightX
+        self.bRY = bottomRightY
+        self.centerX = (self.tLX + self.bRX) /2
+        self.centerY = (self.tLY + self.bRY) / 2
+        
+
 class Card(pygame.sprite.Sprite):  
     '''this is the sprite that contains the card information
        screenSection will contain "player 1 hand or player 2 hand or player 1 score or deck or board?"
@@ -29,5 +40,53 @@ class Card(pygame.sprite.Sprite):
         self.__screenSection = screenSection
         self.imageID = 0
         self.image = pygame.image.load("images/Hanafuda set1/" + str(self.imageID) + ".jpg")
+        self.rect = self.image.get_rect()
         
+        
+class Button(pygame.sprite.Sprite):
+    '''this is a button sprite'''
+    
+    def __init__(self, screen, screenSection, buttonType):
+        
+        pygame.sprite.Sprite.__init__(self)  
+        
+        self.__screen = screen
+        self.screenSection = screenSection
+        self.buttonType = buttonType
+        
+        if buttonType == "Quit":
+            self.image = pygame.image.load("images/buttons/Quit.gif").convert()
+            self.rect = self.image.get_rect()
+            self.rect.centerx = screenSection.centerX
+            self.rect.centery = screenSection.centerY - 100
+            
+    def get_centerx(self):
+        '''this method returns the center x of the tank'''
+        return self.rect.centerx 
+    def get_centery(self):
+        '''this method returns the center y of the tank'''
+        return self.rect.centery   
+            
+    def get_rect_left(self):
+        '''this method returns the left of the tank'''
+        return self.rect.left
+    def get_rect_bottom(self):
+        '''this method returns the bottom of the tank'''
+        return self.rect.bottom  
+    def get_rect_right(self):
+        '''this method returns the right of the tank'''
+        return self.rect.right
+    def get_rect_top(self):
+        '''this method returns the top of the tank'''
+        return self.rect.top
+    
+    def within(self, x, y):
+        print "x:" + str(x)
+        print "y:" + str(y)
+        print "x1:" + str(self.rect.left)
+        print "x2:" + str(self.rect.right)
+        print "y1:" + str(self.rect.top)
+        print "y2:" + str(self.rect.bottom)        
+        return x > self.rect.left and x < self.rect.right and y > self.rect.top and y < self.rect.bottom
+            
         
