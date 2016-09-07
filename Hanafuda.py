e0cc9f58cb8a68f891e707691ba986d35ef297ca
jayyin <jayyin11043@hotmail.com>
2016-09-07 17:23:27 -0400

    setup local player's GUI and interface
    only minor interactions available

--- Hanafuda.py
+++ Hanafuda.py
@@ -49,19 +49,21 @@ def main():
     DeckSS = pySprites.ScreenSection("DeckSS", screen.get_width() / 5 ,screen.get_height() /3,
                                              screen.get_width() / 3, screen.get_height() *2 /3)   
     
-    localPlayer_handSS = pySprites.ScreenSection("localPlayer_handSS", 50 , screen.get_height() *2 /3, 
-                                                 screen.get_width() -10, screen.get_height() -10)
+    localPlayer_handSS = pySprites.ScreenSection("hand", screen.get_width() / 5 , screen.get_height() *2 /3 + 50, 
+                                                 screen.get_width() - 200, screen.get_height())
     
     
     titleBarButtons = []
     MenuButtons = []
-    localPlayer_hand =[]
-    localPlayer_score = []
-    remotePlayer_hand = []
-    remotePlayer_score = []
+    localPlayer_handSprites =[]
+    localPlayer_scoreSprites = []
+    remotePlayer_handSprites = []
+    remotePlayer_scoreSprites = []
     centerBoardSprites = []
     deck = []
     deckSprites = []
+    maxCardsInHand = 8
+    cardHighlights = []
     
     backButton = pySprites.Button(screen,titleBarSS, BACK)
     startGameButton = pySprites.Button(screen,buttonsSS, START)
@@ -167,32 +169,55 @@ def main():
                 deckSprites[:] = []
                 
                 #clearing hands and score for reinisialization
-                localPlayer_hand[:] = []
-                remotePlayer_hand[:] = []
-                localPlayer_score[:] = []
-                remotePlayer_score[:] = []
+                localPlayer_handSprites[:] = []
+                remotePlayer_handSprites[:] = []
+                localPlayer_scoreSprites[:] = []
+                remotePlayer_scoreSprites[:] = []
                 
                 #inisializing deck
-                col = 4
-                row = 2
+                deckCol = 6
+                deckRow = 2
                 tempdeck = range(1,49)
                 deck = random.sample(tempdeck, 48)
                 
                 for i in deck:
                     deckSprites.append(pySprites.Card(screen, DeckSS, i))
                 #initialzing centerBoard
-                centerBoard =  pySprites.Board(centerBoardSS, col, row)
+                centerBoard =  pySprites.Board(centerBoardSS, deckCol, deckRow)
+                localPlayer_handBoard =  pySprites.Board(localPlayer_handSS, maxCardsInHand, 1)
                 
                 #inisializing cards on board
-                for i in range(col * row):
+                for i in range((deckCol-2) * deckRow):
                     centerBoardSprites.append(deckSprites.pop())
                     centerBoardSprites[i].setScreenSection(centerBoard.slots[i][0])
                     centerBoardSprites[i].faceUp = True
                     centerBoardSprites[i].updateImage()
                     centerBoard.slots[i][1] = True
                 
-                gameToBeInit = False
+                #inisializing cards on local hand
+                for i in range(maxCardsInHand):
+                    localPlayer_handSprites.append(deckSprites.pop())
+                    localPlayer_handSprites[i].setScreenSection(localPlayer_handBoard.slots[i][0])
+                    localPlayer_handSprites[i].faceUp = True
+                    localPlayer_handSprites[i].updateImage()
+                    localPlayer_handBoard.slots[i][1] = True
+                    
                 
+                gameToBeInit = False
+            for card in localPlayer_handSprites:
+                mouseX, mouseY = pygame.mouse.get_pos()
+                #print mouseX
+                if card.within(mouseX, mouseY) and not card.mousedOnTF:
+                    #cardHighlights.append([card.cardID,card.mousedOn()])
+                    card.mousedOn()
+                    #print "true"
+                    card.mousedOnTF = True
+                if card.mousedOnTF and  not card.within(mouseX, mouseY):
+                    card.mousedOff()
+                    card.mousedOnTF = False
+            #cardHighlightsGroup =  pygame.sprite.Group(cardHighlights)
+            pygame.display.update()
+            localPlayer_handGroup = pygame.sprite.Group(localPlayer_handSprites)
             centerBoardSpritesGroup = pygame.sprite.Group(centerBoardSprites)
             deckSpritesGroup = pygame.sprite.Group(deckSprites)
             
@@ -217,6 +242,10 @@ def main():
                         game = False                        
                         
             screen.blit(background, (0, 0))
+            #cardHighlightsGroup.draw(screen)
+            #for item in cardHighlights:
+                #item[1].draw(screen)
+            localPlayer_handGroup.draw(screen)
             centerBoardSpritesGroup.draw(screen)
             deckSpritesGroup.draw(screen)
             titleBarGroup.draw(screen)
--- hanafuda_sprites.py
+++ hanafuda_sprites.py
@@ -51,16 +51,16 @@ class Board():
         
         #set (i-... to 0 for both cases below
         #in case the row number or column number is 1
-        if col == 1:
-            colOffSet = 0
-        if row == 1:
-            rowOffSet = 0
-        
+        #if col == 1:
+            #colOffSet = 0
+        #if row == 1:
+            #rowOffSet = 0
+            
         for j in range (1,row + 1):
             for i in range(1,col + 1):
                 
                 #self.slots[slotNumber][0] = ScreenSection("", SS.width * (i - 1) / i, SS.height * (j - 1) / j, SS.width / i, SS.height / j ) incorrect
-                self.slots[slotNumber][0] = ScreenSection("", ((SS.width / col)* (i - rowOffSet))  + SS.tLX, ((SS.height / row) * (j - colOffSet)) + SS.tLY, 
+                self.slots[slotNumber][0] = ScreenSection("", ((SS.width / col)* (i - colOffSet))  + SS.tLX, ((SS.height / row) * (j - rowOffSet)) + SS.tLY, 
                                                           ((SS.width / col)* (i)) + SS.tLX, ((SS.height / row) * (j)) + SS.tLY)
                 self.slots[slotNumber][1] = False
                 #false if the slot is empty
@@ -98,33 +98,57 @@ class Card(pygame.sprite.Sprite):
         #card id 0 is back
         self.cardID = ID
         self.faceUp = False
+        self.mousedOnTF = False
         self.__screenSection = screenSection
+        self.highlightColour = (255,255,0)
         self.month = 0
         self.imageID = 0
         self.cardType = 0
-        self.image = pygame.image.load("images\\Hanafuda set1\\" + str(self.imageID) + ".jpg").convert()
-        self.image = pygame.transform.scale(self.image, (int(self.image.get_rect().width / 2), int (self.image.get_rect().height /2)))
+        self.originalImage = pygame.image.load("images\\Hanafuda set1\\" + str(self.imageID) + ".jpg").convert()
+        self.scaledImage = pygame.transform.scale(self.originalImage, (int(self.originalImage.get_rect().width / 2), int (self.originalImage.get_rect().height /2)))
+        self.image = self.scaledImage
         self.rect = self.image.get_rect()
-        self.rect.centerx = screenSection.tLX
-        self.rect.centery = screenSection.centerY
+        self.rect.left = screenSection.tLX
+        self.rect.top = screenSection.tLY 
+        self.highlight = 0
         
     def setCardID(self, ID):
         self.cardID = ID
+        
+    def mousedOn(self):
+        #self.highlight = pygame.draw.rect(self.__screen,self.highlightColour, self.rect, 1)
+        self.image =  pygame.transform.scale(self.originalImage, (int(self.originalImage.get_rect().width / 1.5), int (self.originalImage.get_rect().height /1.5)))
+        #return self.highlight
+        ##self.image = pygame.transform.scale(self.image, (int(self.image.get_rect().width *1.5), int (self.image.get_rect().height *1.5)))
+        
+    def mousedOff(self):
+        self.image = self.scaledImage
+        #self.highlight = 0
+        ##self.image = pygame.transform.scale(self.image, (int(self.image.get_rect().width /1.5), int (self.image.get_rect().height /1.5)))    
     
     def updateImage(self):
         if self.faceUp and self.imageID != self.cardID:
             self.imageID = self.cardID  
         elif not self.faceUp:
             self.imageID = 0
-        self.image = pygame.image.load("images\\Hanafuda set1\\" + str(self.imageID) + ".jpg").convert()
-        self.image = pygame.transform.scale(self.image, (int(self.image.get_rect().width / 2), int (self.image.get_rect().height /2)))                  
+        self.originalImage = pygame.image.load("images\\Hanafuda set1\\" + str(self.imageID) + ".jpg").convert()
+        self.scaledImage = pygame.transform.scale(self.originalImage, (int(self.originalImage.get_rect().width / 2), int (self.originalImage.get_rect().height /2))) 
+        self.image = self.scaledImage
             
     def setScreenSection(self, screenSection):
         self.__screenSection = screenSection
-        self.rect.centerx = screenSection.tLX
-        self.rect.centery = screenSection.centerY         
+        self.rect.left = screenSection.tLX
+        if screenSection.name == "hand":
+            self.rect.bottom = screenSection.bRY
+        else:
+            self.rect.top = screenSection.tLY  
         
+    def within(self, x, y):       
+        return x >= self.rect.left and x <= self.rect.right and y >= self.rect.top and y <= self.rect.bottom    
     #def update(self):
+    
+    def match(self, card):
+        self.cardID
         
 class Button(pygame.sprite.Sprite):
     '''this is a button sprite'''
